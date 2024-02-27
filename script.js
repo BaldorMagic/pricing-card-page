b52f5c83b060767d8fa1685e1abe45cb2cd5367f
jneveu <james@baldor.dev>
2024-02-27 14:17:45 +0100

    display flex, margin, line height ajustments

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// write your JavaScript here
+/*function checkFunction() {
+  var checkBox = document.getElementById("myCheck");
+  var text = document.getElementById("anually");
+  if (checkBox.checked == true){
+    text.style.fontWeight = "600";
+  } else {
+     text.style.fontWeight = "400";
+  }
+}
